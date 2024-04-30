4810d4f8b3d2c8d9c059f06b088123d9daba0161
Nicholas Curl <nicholas_curl@na.honda.com>
2024-04-30 09:29:57 -0400

    Added saving settings to file on window close

--- src/window/imp.rs
+++ src/window/imp.rs
@@ -1,42 +1,44 @@
 // Module: window
+use std::cell::{Cell, RefCell};
+use std::fs::File;
+use std::io::Write;
+
 use adw::subclass::prelude::*;
 use glib::subclass::InitializingObject;
+use gtk::glib::types::StaticTypeExt;
 use gtk::prelude::*;
-use gtk::{
-    gio::{self, Settings},
-    glib::{self, types::StaticTypeExt},
-    CompositeTemplate, Expander, ListBox, Notebook,
-};
-use std::cell::{Cell, OnceCell, RefCell};
+use gtk::{gio, glib, CompositeTemplate, Expander, ListBox, Notebook};
+use toml_edit::{table, value, DocumentMut};
 
 use crate::skeleton::Skeleton;
+use crate::utils::settings_path;
 
 #[derive(CompositeTemplate, Default)]
 #[template(resource = "/com/nc/calculator/window.ui")]
 pub struct Window {
     #[template_child]
-    pub mem_hist_list: TemplateChild<ListBox>,
+    pub mem_hist_list:     TemplateChild<ListBox>,
     #[template_child]
-    pub tabs: TemplateChild<Notebook>,
+    pub tabs:              TemplateChild<Notebook>,
     #[template_child]
-    pub expander_keypad: TemplateChild<Expander>,
+    pub expander_keypad:   TemplateChild<Expander>,
     #[template_child]
-    pub expander_history: TemplateChild<Expander>,
+    pub expander_history:  TemplateChild<Expander>,
     #[template_child]
-    pub expander_convert: TemplateChild<Expander>,
+    pub expander_convert:  TemplateChild<Expander>,
     #[template_child]
-    pub keypad_buttons: TemplateChild<Skeleton>,
+    pub keypad_buttons:    TemplateChild<Skeleton>,
     pub persistent_keypad: Cell<bool>,
-    pub mem_hist: RefCell<Option<gio::ListStore>>,
-    pub settings: OnceCell<Settings>,
+    pub mem_hist:          RefCell<Option<gio::ListStore>>,
 }
 
 #[glib::object_subclass]
 impl ObjectSubclass for Window {
+    type ParentType = adw::ApplicationWindow;
+    type Type = super::Window;
+
     // `NAME` needs to match `class` attribute of template
     const NAME: &'static str = "MainWindow";
-    type Type = super::Window;
-    type ParentType = adw::ApplicationWindow;
 
     fn class_init(klass: &mut Self::Class) {
         Skeleton::ensure_type();
@@ -64,7 +66,8 @@ impl Window {
                     self.expander_convert.set_expanded(false);
                 }
             }
-        } else {
+        }
+        else {
             self.show_keypad_widget(false);
         }
     }
@@ -80,7 +83,8 @@ impl Window {
             if self.expander_convert.is_expanded() {
                 self.expander_convert.set_expanded(false);
             }
-        } else if !self.expander_convert.is_expanded() {
+        }
+        else if !self.expander_convert.is_expanded() {
             self.show_tabs(false);
         }
     }
@@ -96,7 +100,8 @@ impl Window {
             if self.expander_history.is_expanded() {
                 self.expander_history.set_expanded(false);
             }
-        } else if !self.expander_history.is_expanded() {
+        }
+        else if !self.expander_history.is_expanded() {
             self.show_tabs(false);
         }
     }
@@ -115,7 +120,8 @@ impl Window {
         if persistent_keypad && self.expander_convert.is_expanded() {
             if do_show {
                 h += 6;
-            } else {
+            }
+            else {
                 h -= 6;
             }
         }
@@ -124,14 +130,16 @@ impl Window {
             let kb_h = self.keypad_buttons.height();
             if kb_h > 10 {
                 h += kb_h + 9;
-            } else {
+            }
+            else {
                 h += 9;
             }
             if !persistent_keypad {
                 self.tabs.set_visible(false);
             }
             self.obj().set_default_size(w, h);
-        } else {
+        }
+        else {
             h -= self.keypad_buttons.height();
             self.keypad_buttons.set_visible(false);
             self.obj().set_default_size(w, h);
@@ -156,14 +164,16 @@ impl Window {
             let t_h = self.tabs.height();
             if t_h > 10 {
                 h += t_h + 9;
-            } else {
+            }
+            else {
                 h += 9;
             }
             if !persistent_keypad {
                 self.keypad_buttons.set_visible(false);
             }
             self.obj().set_default_size(w, h);
-        } else {
+        }
+        else {
             h -= self.tabs.height();
             self.tabs.set_visible(false);
             self.obj().set_default_size(w, h);
@@ -183,7 +193,6 @@ impl ObjectImpl for Window {
         self.tabs.set_visible(false);
 
         let obj = self.obj();
-        obj.setup_settings();
         obj.setup_mem_hist();
         obj.create_rows();
     }
@@ -193,7 +202,28 @@ impl ObjectImpl for Window {
 impl WidgetImpl for Window {}
 
 // Trait shared by all windows
-impl WindowImpl for Window {}
+impl WindowImpl for Window {
+    fn close_request(&self) -> glib::Propagation {
+        // Save settings
+        let mut settings_table = table();
+        settings_table["persistent_keypad"] = value(self.persistent_keypad.get());
+        let mut window_settings = table();
+        window_settings["height"] = value(i64::try_from(self.obj().height()).expect("Cannot convert height to i64"));
+        window_settings["width"] = value(i64::try_from(self.obj().width()).expect("Cannot convert width to i64"));
+        window_settings["is_maximized"] = value(self.obj().is_maximized());
+
+        let mut doc = DocumentMut::new();
+        doc.insert("settings", settings_table);
+        doc.insert("window", window_settings);
+
+        let mut file = File::create(settings_path()).expect("Failed to create settings file");
+        file.write(doc.to_string().as_bytes())
+            .expect("Failed to write settings file");
+
+        // Pass close request on to the parent
+        self.parent_close_request()
+    }
+}
 
 // Trait shared by all application windows
 impl ApplicationWindowImpl for Window {}
