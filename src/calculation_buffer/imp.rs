10d80f95d5dda8f2197f704dbde3f4e9958798c5
Nicholas Curl <nicholas_curl@na.honda.com>
2024-05-15 08:54:43 -0400

    Formatting

--- src/calculation_buffer/imp.rs
+++ src/calculation_buffer/imp.rs
@@ -1,19 +1,19 @@
 //! Implementation of the calculation buffer.
 
-use gtk::{glib,EntryBuffer};
 use gtk::subclass::prelude::*;
+use gtk::{glib, EntryBuffer};
 
-/// The calculation buffer. It is a subclass of [`gtk::EntryBuffer`] and is used to store the calculation that the user is currently inputting.
-
+/// The calculation buffer. It is a subclass of [`gtk::EntryBuffer`] and is used to store the calculation that the user
+/// is currently inputting.
 #[derive(Debug, Default)]
-pub struct CalculationBuffer {
-}
+pub struct CalculationBuffer {}
 
 #[glib::object_subclass]
 impl ObjectSubclass for CalculationBuffer {
-    const NAME: &'static str = "CalculationBuffer";
-    type Type = super::CalculationBuffer;
     type ParentType = EntryBuffer;
+    type Type = super::CalculationBuffer;
+
+    const NAME: &'static str = "CalculationBuffer";
 }
 
 impl ObjectImpl for CalculationBuffer {}
