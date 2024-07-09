9da6352642684facb384dba329a48c4082cbfb99
Nicholas Curl <nicholas_curl@na.honda.com>
2024-07-09 07:35:58 -0500

    Commented out unnecessary grid components

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,20 +11,20 @@ module.exports = {
   theme: {
     extend: {
       gridTemplateAreas: {
-        "assembly-part": ["select assy-no part-name"],
-        "assembly-no-detail": ["base dash1 model dash2 revision dash3 status"],
-        "assembly-parts": ["part-no latest-revision part-name bl sec item qty hns"],
-        "mto-app": ["model type options"],
-        "mto-app-detail": ["app0 app1 app2 app3 app4 app5 app6 app7 app8 app9"],
+        // "assembly-part": ["select assy-no part-name"],
+        // "assembly-no-detail": ["base dash1 model dash2 revision dash3 status"],
+        // "assembly-parts": ["part-no latest-revision part-name bl sec item qty hns"],
+        // "mto-app": ["model type options"],
+        // "mto-app-detail": ["app0 app1 app2 app3 app4 app5 app6 app7 app8 app9"],
       },
       gridTemplateColumns: {
-        "assembly-part": "auto minmax(0, 1fr) minmax(0, 1fr)",
-        "assembly-no-detail": "minmax(0, 1fr) auto minmax(0, 1fr) auto minmax(0, 1fr) auto minmax(0, 1fr)",
-        "assembly-parts":
-          "minmax(11rem,1fr) minmax(7rem,auto) minmax(14.25rem,1fr) minmax(1.5rem,auto) minmax(3.5rem,auto) minmax(4.5rem,auto) minmax(2.5rem,auto) minmax(2.25rem,auto)",
-        "mto-app": "minmax(3.5rem,1fr) minmax(3.5rem,1fr) minmax(20rem,1fr)",
-        "mto-app-detail":
-          "minmax(2rem,auto) minmax(2rem,auto) minmax(2rem,auto) minmax(2rem,auto) minmax(2rem,auto) minmax(2rem,auto) minmax(2rem,auto) minmax(2rem,auto) minmax(2rem,auto) minmax(2rem,auto)",
+        // "assembly-part": "auto minmax(0, 1fr) minmax(0, 1fr)",
+        // "assembly-no-detail": "minmax(0, 1fr) auto minmax(0, 1fr) auto minmax(0, 1fr) auto minmax(0, 1fr)",
+        // "assembly-parts":
+        //   "minmax(11rem,1fr) minmax(7rem,auto) minmax(14.25rem,1fr) minmax(1.5rem,auto) minmax(3.5rem,auto) minmax(4.5rem,auto) minmax(2.5rem,auto) minmax(2.25rem,auto)",
+        // "mto-app": "minmax(3.5rem,1fr) minmax(3.5rem,1fr) minmax(20rem,1fr)",
+        // "mto-app-detail":
+        //   "minmax(2rem,auto) minmax(2rem,auto) minmax(2rem,auto) minmax(2rem,auto) minmax(2rem,auto) minmax(2rem,auto) minmax(2rem,auto) minmax(2rem,auto) minmax(2rem,auto) minmax(2rem,auto)",
       },
     },
   },
