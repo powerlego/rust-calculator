5452c9a444c8add2407fd659076e363f1d8f621a
Nicholas Curl <nicholas_curl@na.honda.com>
2024-07-15 07:50:16 -0500

    Added scaffolding for other elements

--- src/renderer/src/App.svelte
+++ src/renderer/src/App.svelte
@@ -1,16 +1,28 @@
 <script lang="ts">
   import BasicNumpad from "./components/BasicNumpad.svelte";
   import type { ComponentType } from "svelte";
+  import Display from "./components/Display.svelte";
   const numpad: ComponentType = BasicNumpad;
 </script>
 
-<div class="flex flex-col w-full h-full">
+<div class="relative flex flex-col w-full h-full">
   <!-- Calculator Type select -->
-  <div></div>
+  <div class="flex-grow-0 h-12 bg-green-800 min-h-12"></div>
+  <div class="min-h-0 bg-white expression"></div>
+
   <!-- Input Display -->
-  <div></div>
+  <Display />
   <!-- Memory Functions -->
-  <div></div>
+  <div class="min-h-0 bg-blue-600 memory"></div>
   <!-- Number Pad -->
-  <svelte:component this={numpad} />
+  <svelte:component this={numpad} class="min-h-0 w-[calc(100%-6px)] mx-[3px] mb-[3px]" />
 </div>
+
+<style>
+  .expression {
+    flex: 0.25 3 22px;
+  }
+  .memory {
+    flex: 0.25 3 32px;
+  }
+</style>
